dd20166bd61c960640cde55b91b13639b0fe8e7d
huekoh <huexkoh@gmail.com>
2024-04-23 09:40:05 +0800

    Add author controller and service

---
+++ src/main/java/guru/springframework/spring6webapp/services/AuthorServiceImpl.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6webapp.services;
+
+import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    private final AuthorRepository authorRepository;
+
+    @Override
+    public Iterable<Author> findAll() {
+        return authorRepository.findAll();
+    }
+
+}
